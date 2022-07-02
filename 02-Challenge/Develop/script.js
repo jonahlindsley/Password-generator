aaa07d6868e7cc3864ef134f5f5c4458d5d91781
jonah lindsley <jonahlindsley@yahoo.com>
2022-07-01 21:38:38 -0500

    updated commenting

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -13,9 +13,8 @@ var specialArray = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
 var numberArray = ['123456789']
 
 
-
+//links to the button from the html
 var generateBtn = document.querySelector("#generate");
-
 
 
 //the main function
@@ -23,7 +22,7 @@ function generatePassword() {
 
 
 
-
+// sets the empty variables
 var passwordRequirements = ''
 var generatedPassword = ''
 
@@ -32,17 +31,24 @@ var generatedPassword = ''
 
 
 
-//the prompts for what to make the password with
+// prompt to chose a password length
   var passwordLength = prompt('input length between 8 and 128');
-
-  if (passwordLength < 8){
-  alert('woah now buddy you need a password of least 8 to continue')        
-  return}
-  if (passwordLength > 128){
-    alert("easy there slugger, your password can't be over 128 long!")
+// checks to make sure the length is alright and if it isnt alerts to tell you whats wrong
+  if (passwordLength < 8 || passwordLength > 128){
+    alert('woah now buddy you need a password of least 8 to continue')        
     return
   }
+  // if (passwordLength > 128){
+  //   alert("easy there slugger, your password can't be over 128 long!")
+  //   return
+  // }
+  // if (passwordLength){
+  //   alert("sorry buddy, you can only input numbers here, not letters!")
+  //   console.log(passwordLength.charAt[1])
+  //   return
+  // }
 
+//the prompts for what to make the password with
   var numbers = confirm('would you like to use numbers?');
   var uppercase = confirm('would you like uppercase letters?');
   var lowercase = confirm('would you like lowercase letters?');
@@ -54,23 +60,25 @@ var generatedPassword = ''
 console.log(passwordLength);
 
 //alert if they didnt say yes to anything
-if (numbers, uppercase, lowercase, special === false){
+if (!numbers && !uppercase && !lowercase && !special){
   alert('wrong call friendo, you need to add somthing or what will i make your password with?')
   return
 }
+// if they selected numbers then this is added
 if (numbers){
 passwordRequirements = passwordRequirements.concat(numberArray)
- 
+//  if they selected uppercase then this is added
 }
 if (uppercase){
     passwordRequirements = passwordRequirements.concat(upperLetterArry)
   
     }
-
+ //   if they selected lowercase then this is added
  if (lowercase){
   passwordRequirements = passwordRequirements.concat(lowerLetterArry)
  
 } 
+// if they selected special character then this is added
 if (special){
  passwordRequirements = passwordRequirements.concat(specialArray)
  }
@@ -80,8 +88,7 @@ if (special){
   var randomPassword = passwordRequirements.charAt(Math.floor(Math.random() * passwordRequirements.length));
   generatedPassword = generatedPassword.concat(randomPassword)
 }
-
-console.log(generatedPassword)
+//returns the new password
 return generatedPassword;
 }
 
@@ -93,5 +100,5 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
+//adds the function to the button
 generateBtn.addEventListener("click", writePassword);
--- 02-Challenge/README.md
+++ 02-Challenge/README.md
@@ -2,4 +2,4 @@ this week i made a random password generator
 it selects a custom password based on the inputs you provide it and prints to the screen
 
 [deployed website](https://jonahlindsley.github.io/randomPassword/)
-![The Password Generator application deployed.](screenshot.jpeg)
+C:\Users\jonah\homework\password-gen\screenshot.jpeg
